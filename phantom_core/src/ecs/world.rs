d51e793638f3574333cd7b68c7c09a5b2c89476a
Gand Shack <gandshack89mc@gmail.com>
2026-04-02 17:14:45 -0500

    implement World::get_component_mut<C>()

--- phantom_core/src/ecs/world.rs
+++ phantom_core/src/ecs/world.rs
@@ -21,11 +21,6 @@ impl World {
     }
 
     pub fn spawn(&mut self) -> u32 {
-        // Make sure sparse set for transforms exist if not create it
-        self.sparse_set_storage
-            .entry(TypeId::of::<Transform>())
-            .or_insert_with(|| Box::new(SparseSet::<Transform>::new()));
-
         // Generate entity_id
         let entity_id = self.deleted_entity_ids.pop().unwrap_or_else(|| {
             let id = self.next_entity_id;
@@ -41,18 +36,16 @@ impl World {
     // pub fn destroy(entity_id) {}
 
     pub fn add_component<C: Any + 'static>(&mut self, entity_id: u32, component: C) {
+        // Make sure sparse set for C exists if not create it
         self.sparse_set_storage
             .entry(TypeId::of::<C>())
             .or_insert_with(|| Box::new(SparseSet::<C>::new()));
 
-        let sparse_set = self
-            .sparse_set_storage
-            .get_mut(&TypeId::of::<C>())
-            .expect("Component sparse set not found")
-            .downcast_mut::<SparseSet<C>>()
-            .expect("Downcast failed");
-
-        sparse_set.insert(entity_id, component);
+        if let Some(sparse_set) = self.sparse_set_storage.get_mut(&TypeId::of::<C>()) {
+            if let Some(sparse_set) = sparse_set.downcast_mut::<SparseSet<C>>() {
+                sparse_set.insert(entity_id, component);
+            }
+        }
     }
 
     // pub fn remove_component<C>(entity_id, component){}
@@ -62,7 +55,13 @@ impl World {
             .and_then(|sparse_set| sparse_set.downcast_ref::<SparseSet<C>>())
             .and_then(|sparse_set| sparse_set.get(entity_id))
     }
-    // pub fn get_component_mut<C>(entity_id, component) Option<&mut C> {}
+
+    pub fn get_component_mut<C: Any + 'static>(&mut self, entity_id: u32) -> Option<&mut C> {
+        self.sparse_set_storage
+            .get_mut(&TypeId::of::<C>())
+            .and_then(|sparse_set| sparse_set.downcast_mut::<SparseSet<C>>())
+            .and_then(|sparse_set| sparse_set.get_mut(entity_id))
+    }
     // pub fn has_component<C>(entity_id, component) -> bool {}
     // pub fn query_with<C>() -> Vec<entity_id> {}
     // pub fn query_with2<A,B>() -> Vec<entity> {}
@@ -122,5 +121,24 @@ mod tests {
         world.add_component(entity, Transform::default());
         let transform = world.get_component::<Transform>(entity).unwrap();
         assert_eq!(transform.position, Vec3::ZERO);
+    }
+
+    #[test]
+    fn check_get_component_mut() {
+        let mut world = World::new();
+        let entity = world.spawn();
+        world.add_component(entity, Transform::default());
+        let transform = world.get_component_mut::<Transform>(entity).unwrap();
+        transform.position = Vec3 {
+            x: 10.0,
+            y: 10.0,
+            z: 10.0,
+        };
+        let comparison_vec = Vec3 {
+            x: 10.0,
+            y: 10.0,
+            z: 10.0,
+        };
+        assert_eq!(transform.position, comparison_vec);
     }
 }
